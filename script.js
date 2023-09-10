e311dbd428e2cb3ab5ef32d2bb36fe6ccec9fa61
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-09-10 18:16:29 +0100

    added functionalities

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 let displayValues = document.querySelectorAll(".num");
-let interval = 1000;
+let interval = 4000;
 
 console.log(displayValues);
 
 displayValues.forEach((displayValue) => {
     let startValue = 0;
-    let endValue = parseInt(displayValue.getAttribute("data-val"))
+    let endValue = parseInt(displayValue.getAttribute("data-val"));
     console.log(endValue);
+    // Math.floor round down a number to the nearest integer less than or equal to the given number.
+    let duration = Math.floor( interval / endValue );
+    let counter = setInterval(function(){
+        startValue += 1;
+        displayValue.textContent = startValue;
+        if(startValue === endValue){
+            clearInterval(counter)
+        }
+    }, duration);
 })
